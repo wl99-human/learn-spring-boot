c20dec2a4a9135f59814d50295e12914caa8bf40
wl99-human <weileelim2012@gmail.com>
2023-11-13 23:35:27 +0800

    Create a simple API

--- src/main/java/com/example/learn/LearnApplication.java
+++ src/main/java/com/example/learn/LearnApplication.java
@@ -2,12 +2,21 @@ package com.example.learn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class LearnApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearnApplication.class, args);
 	}
 
+	@GetMapping
+	public List<String> hello() {
+		return List.of("Hello", "World");
+	}
 }
