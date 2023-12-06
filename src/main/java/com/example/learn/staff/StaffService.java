08592d14e304824dda86c5c91b1419e024a45e3d
wl99-human <weileelim2012@gmail.com>
2023-12-06 21:36:39 +0800

    JPA repository

--- src/main/java/com/example/learn/staff/StaffService.java
+++ src/main/java/com/example/learn/staff/StaffService.java
@@ -1,14 +1,19 @@
 package com.example.learn.staff;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @Service
 public class StaffService {
+    private final StaffRepository staffRepository;
+
+    @Autowired
+    public StaffService(StaffRepository staffRepository) {
+        this.staffRepository = staffRepository;
+    }
     public List<Staff> getStaff() {
-        return List.of(new Staff(1L,"John", "johndabest@gmail.com",LocalDate.of(1999, Month.APRIL, 1), 24));
+        return staffRepository.findAll();
     }
 }
